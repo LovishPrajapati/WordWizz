79ffb197f212145b114408ff147dec677e3916a4
LovishPrajapati <prajapatilovish@gmail.com>
2020-11-09 09:30:19 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Search></Search>
+    </div>
+  );
+};
+
+export default App;
